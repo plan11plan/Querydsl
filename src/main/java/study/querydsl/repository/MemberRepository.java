861036c373458dd98244c005243c034c6415a667
plan11plan <pmsuk7028@naver.com>
2023-02-03 19:24:22 +0900

    fixing

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 /**
  * 스프링 데이터 JPA 를 사용한 Repository
  */
-public interface MemberRepository extends JpaRepository<Member,Long>,MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member,Long>,MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     //select m from Member m where m.username =?
     List<Member> findByUsername(String username);
